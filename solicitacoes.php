681752056991679cea78d5824e09ad9d901a677a
rafaelcarvalho-git <rafaskyplay@gmail.com>
2022-05-03 01:02:41 -0300

    commit

--- solicitacoes.php
+++ solicitacoes.php
@@ -16,7 +16,7 @@
 <body class="bg-light">
 <nav class="navbar navbar-light bg-primary">
     <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#myModalCad">Cadastrar Usuário</button>
-    <a href="usuarios_cadastrados.php"><button type="button" class="btn btn-success">Usuários Cadastrados</button></a> 
+    <a href="usuarios_cadastrados.php"><button type="button" class="btn btn-success">Usuários</button></a> 
 </nav>
 <!-- Janela Cadastrar Usuário -->
 <div class="modal fade" id="myModalCad" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
