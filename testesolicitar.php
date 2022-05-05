53b7a533acc12e4f1df5870febe6d6b1c865ef95
rafaelcarvalho-git <rafaskyplay@gmail.com>
2022-05-05 13:01:44 -0300

    commit

--- testesolicitar.php
+++ testesolicitar.php
@@ -2,6 +2,6 @@
     include_once('conexao.php');
     $result_solicitar = "INSERT INTO solicitacoes(tipo_certificado, nome, cpf, data_nascimento, email, telefone, cep, endereco, observacoes, data_solicitacao, contador, documentos) 
     VALUES 
-    ('R$ 135,00 E-CPF A1 Mídia Digital', 'RAFAEL CANDIDO LACERDA CARVALHO', '07141342362', '2000-05-18', 'rafaelcarvalho@gmail.com', '88988573004', '63024380', 'RUA ENGENHEIRO JOSE ONOFRE MARQUES, 11 - SAO JOSE', 'Atender por videoconferencia', NOW(), 'GEONE', 'documentos')";
+    ('R$ 135,00 E-CPF A1 Mídia Digital', 'RAFAEL CANDIDO LACERDA CARVALHO', '17141342362', '2000-05-18', 'rafaelcarvalho@gmail.com', '88988573004', '63024380', 'RUA ENGENHEIRO JOSE ONOFRE MARQUES, 11 - SAO JOSE', 'Atender por videoconferencia', NOW(), 'GEONE', 'documentos')";
 	$resultado_solicitar= mysqli_query($connect, $result_solicitar);       
 ?>
