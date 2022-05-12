86b681a1a0a7e9b0c166212a33e74dd7c2ada92b
rafaelcarvalho-git <rafaskyplay@gmail.com>
2022-05-12 17:50:06 -0300

    commit

--- cadastrar_usuario.php
+++ cadastrar_usuario.php
@@ -1,6 +1,13 @@
 <?php
   session_start();
 	include_once('conexao.php');	
+  if((!isset($_SESSION['usuario']) == true) or (!isset($_SESSION['senha']) == true) or (!isset($_SESSION['privilegio']) == true)) {
+    unset($_SESSION['usuario']);
+    unset($_SESSION['senha']);
+    unset($_SESSION['privilegio']);
+    $_SESSION['msgLogin'] = "<div class='alert alert-danger alert-dismissible fade show' role='alert'>Acesso restrito!<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button></div>"; 
+    header('Location: login.php');
+  }
   $usuario = strtoupper($_POST['usuario']);
   $senha = $_POST['senha'];
   $privilegio = $_POST['privilegio'];	
--- excluir_solicitacao.php
+++ excluir_solicitacao.php
@@ -1,6 +1,13 @@
 <?php
   session_start();
   include_once("conexao.php");
+  if((!isset($_SESSION['usuario']) == true) or (!isset($_SESSION['senha']) == true) or (!isset($_SESSION['privilegio']) == true)) {
+    unset($_SESSION['usuario']);
+    unset($_SESSION['senha']);
+    unset($_SESSION['privilegio']);
+    $_SESSION['msgLogin'] = "<div class='alert alert-danger alert-dismissible fade show' role='alert'>Acesso restrito!<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button></div>"; 
+    header('Location: login.php');
+  }
   $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
   if (isset($id)) {
     $confirmaId = true;
