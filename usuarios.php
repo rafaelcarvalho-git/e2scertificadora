f867ef3f6472ad497fc66a1d180b961b5ed6e810
rafaelcarvalho-git <rafaskyplay@gmail.com>
2022-05-19 17:08:36 -0300

    fix permissao acesso

--- usuarios.php
+++ usuarios.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     include_once('conexao.php');
-    if(isset($_SESSION['privilegio']) != 'Administrador'){
+    if(isset($_SESSION['privilegio']) == true and $_SESSION['privilegio'] != 'Administrador'){
       $_SESSION['msgLogin'] = "<div class='alert alert-danger alert-dismissible fade show' role='alert'>Acesso somente para Administradores!<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button></div>"; 
       header("Location: index.php");
     }
