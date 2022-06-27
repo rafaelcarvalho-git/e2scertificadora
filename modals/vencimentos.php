3b731b7410a8ad74afb85e1b96286d3a8d1c3ed5
rafaelcarvalho-git <rafaskyplay@gmail.com>
2022-06-27 10:52:30 -0300

    commit

--- modals/vencimentos.php
+++ modals/vencimentos.php
@@ -21,7 +21,48 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
     </div>
     <div class="modal-body">
-
+    <table class="table table-hover">
+            <thead class="thead-dark">
+              <tr>
+                <th scope="col">Cliente</th>
+                <th scope="col">Certificado</th>
+                <th scope="col">Telefone</th>
+                <th scope="col">Conclusao</th>
+                <th scope="col">Usuário</th>
+                <th scope="col">Vencimento</th>
+                <th scope="col">Renovar</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>THIAGO SAMPAIO DA SILVA</td>
+                <td>R$ 135,00 E-CPF A3 sem mídia 1 ano</td>
+                <td><a href="">88996444627</a></td>
+                <td>2021-03-03 14:55:07</td>                  
+                <td>GEONE</td>
+                <td>2022-04-03 14:55:07</td>                  
+                <td><button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#concluirSolicitacao"><i class="bi bi-check2-circle"></i></button></td>
+              </tr>
+          <!-- Janela Confirma Renovar Solicitação -->
+          <div class="modal fade" id="concluirSolicitacao<?php echo $rows_solicitacoes['id']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Concluir Solicitação</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  Deseja concluir a solicitação de <?php echo $rows_solicitacoes['nome']; ?>? <br>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                  <?php echo "<a href='concluir_solicitacao.php?id=" . $rows_solicitacoes['id'] . "' style='color: white;'><button type='button' class='btn btn-primary'>Concluir</button></a>";?>
+                </div>
+              </div>
+            </div>
+          </div><?php } ?>
+          </tbody>
+          </table>
     </div>
     </div>
 </div>
