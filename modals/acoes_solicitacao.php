5509f4321b56edd8724dc7915a5dfb011f0f4b6b
rafaelcarvalho-git <rafaskyplay@gmail.com>
2022-06-28 10:38:32 -0300

    commit

--- modals/acoes_solicitacao.php
+++ modals/acoes_solicitacao.php
@@ -60,6 +60,7 @@
         </div>
         <div class="modal-body">
           Deseja concluir a solicitação de <?php echo $rows_solicitacoes['nome']; ?>? <br>
+          <div class="d-flex">Renovar a solicitação em <input class="form-control mx-2" type="number" min=1 max=3 style="width:75px"> anos.</div>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
